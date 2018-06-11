32350d2e13ce459d5de73c0138d5cf3416b470f0
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-11 19:23:46 +0100

    adds all cats in using method

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
 var app = function(){
+  addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
   addCat("Inbred Cat", "Toast", "https://i.pinimg.com/originals/22/7f/b1/227fb15938dfe0b60978072e9e7f61b3.jpg");
+  addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+  addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
 }
 
 var addCat = function(name, food, url){
@@ -20,13 +23,13 @@ var createUl = function(){
 
 var createName = function(name){
   var catName = document.createElement('li');
-  catName.textContent = name;
+  catName.textContent = "Name: " + name;
   return catName;
 }
 
 var createFood = function(food){
   var favFood = document.createElement('li');
-  favFood.textContent = food;
+  favFood.textContent = "Favourite food: " + food;
   return favFood;
 }
 var createImage = function(){
