246589896dc4ad9db330adffd458f53385385d15
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-11 18:27:44 +0100

    adds images

--- public/app.js
+++ public/app.js
@@ -1,6 +1,4 @@
 var app = function(){
-  // var img = document.createElement('img');
-  // img.classList.add('img');
 
   var cat = document.createElement('ul');
   cat.classList.add('cat')
@@ -11,11 +9,22 @@ var app = function(){
   var favFood = document.createElement('li');
   favFood.textContent = "New Favourite Food";
 
+  var img = document.createElement('img');
+  img.src = "https://i.pinimg.com/originals/22/7f/b1/227fb15938dfe0b60978072e9e7f61b3.jpg"
+  img.width = 500;
+
+  var image = document.createElement('li');
+
   var cats = document.querySelector("#cats");
+
   cats.appendChild(cat);
 
-  cat.appendChild(name);
-  cat.appendChild(favFood);
+    cat.appendChild(name);
+    cat.appendChild(favFood);
+    cat.appendChild(image);
+      image.appendChild(img);
+
+
 }
 
 window.onload = app;
