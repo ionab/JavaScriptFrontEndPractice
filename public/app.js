d806330700cd33720f2fb51cee9b14949ff5c6c1
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-11 19:10:37 +0100

    refactored to make it easy to add a cat

--- public/app.js
+++ public/app.js
@@ -1,30 +1,56 @@
 var app = function(){
+  addCat("Inbred Cat", "Toast", "https://i.pinimg.com/originals/22/7f/b1/227fb15938dfe0b60978072e9e7f61b3.jpg");
+}
 
+var addCat = function(name, food, url){
+  var catul = createUl();
+  var catName = createName(name);
+  var catfood = createFood(food);
+  var imageli = createImage();
+  var url = createImg(url);
+
+  appendElements(catul, catName, catfood, imageli, url);
+}
+
+var createUl = function(){
   var cat = document.createElement('ul');
   cat.classList.add('cat')
+  return cat;
+}
 
-  var name = document.createElement('li');
-  name.textContent = "Cat Name";
+var createName = function(name){
+  var catName = document.createElement('li');
+  catName.textContent = name;
+  return catName;
+}
 
+var createFood = function(food){
   var favFood = document.createElement('li');
-  favFood.textContent = "New Favourite Food";
-
-  var img = document.createElement('img');
-  img.src = "https://i.pinimg.com/originals/22/7f/b1/227fb15938dfe0b60978072e9e7f61b3.jpg"
-  img.width = 500;
-
+  favFood.textContent = food;
+  return favFood;
+}
+var createImage = function(){
   var image = document.createElement('li');
+  return image;
+}
+var createImg = function(url){
+  var img = document.createElement('img');
+  img.src = url;
+  img.width = 500;
+  return img;
+}
 
+var appendElements = function(cat, name, food, image, img){
   var cats = document.querySelector("#cats");
-
   cats.appendChild(cat);
 
     cat.appendChild(name);
-    cat.appendChild(favFood);
+    cat.appendChild(food);
     cat.appendChild(image);
       image.appendChild(img);
 
 
 }
+
 
 window.onload = app;
